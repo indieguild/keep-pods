b3ea12be8d20d123349f2b7cf6487f2b44491d0a
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-12-18 02:27:35 +0530

    added abis
    Signed-off-by: abhimanyu121 <abhimanyushekhawat17.as@gmail.com>

---
+++ packages/contracts/src/abis/StakingPool.js
@@ -0,0 +1,31 @@
+[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_extraData",
+				"type": "bytes"
+			}
+		],
+		"name": "approveAndCall",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,8 +2,10 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   Keep: "0xb64649fe00f8Ef5187d09d109C6F38f13C7CF857",
-  stakingContract : "0x234d2182B29c6a64ce3ab6940037b5C8FdAB608e"
-
+  sKEEP: "0xac437a49ab06ae631d5b2600169a4202bcde4419",
+  stakingContract : "0x234d2182B29c6a64ce3ab6940037b5C8FdAB608e",
+  Registry : "0xAf70ae35AaC91fC67E4bB8b204542a3E4c57E3cA",
+  StakingPool : "0x68f4dfa41d33ae18f22de5e35a61455fd43239f5"
 };
 
 export default addresses;
